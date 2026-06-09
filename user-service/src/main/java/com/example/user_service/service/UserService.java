31741a1895a18499e8e08f5dffdc9c19d7fa0c67
Jonathan <jontyrif@gmail.com>
2026-06-09 15:19:06 +0530

    Successfully implemented the createUser feature of the user-service

---
+++ user-service/src/main/java/com/example/user_service/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.user_service.service;
+
+import com.example.user_service.dto.UserDto;
+import com.example.user_service.entity.User;
+import com.example.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDto createUser(UserDto input){
+        log.info("Creating user: {}", input);
+
+        final User createdUser = User.builder()
+                .name(input.getName())
+                .surname(input.getSurname())
+                .email(input.getEmail())
+                .address(input.getAddress())
+                .alerting(input.isAlerting())
+                .energyAlertingThreshold(input.getEnergyAlertingThreshold())
+                .build();
+
+        final User saved = userRepository.save(createdUser);
+
+        return toDto(saved);
+    }
+
+    private UserDto toDto(User user){
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .alerting(user.isAlerting())
+                .energyAlertingThreshold(user.getEnergyAlertingThreshold())
+                .build();
+    }
+}
