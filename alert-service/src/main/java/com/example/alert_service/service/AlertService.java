cede6ec1052d0c56f6e2c5041c2993b250fe266d
Jonathan <jontyrif@gmail.com>
2026-06-16 12:24:44 +0530

    Created an AlertService to be able to handle receiving messages from the usage-service

---
+++ alert-service/src/main/java/com/example/alert_service/service/AlertService.java
@@ -0,0 +1,17 @@
+package com.example.alert_service.service;
+
+import com.example.kafka.event.AlertingEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AlertService {
+
+    @KafkaListener(topics = "energy-alerts", groupId = "alert-service")
+    public void energyUsageAlertEvent(AlertingEvent alertingEvent) {
+        log.info("Received alert event: {}", alertingEvent);
+
+    }
+}
