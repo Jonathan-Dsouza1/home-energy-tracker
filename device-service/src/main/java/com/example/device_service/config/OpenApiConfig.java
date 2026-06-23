381d1efb22392cecf0606470402e8c1a8d7d9a10
Jonathan <jontyrif@gmail.com>
2026-06-23 14:37:13 +0530

    Configured the OpenApiConfig file in different services and also added the openapi ui in the pom files of the same services

---
+++ device-service/src/main/java/com/example/device_service/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.example.device_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI deviceServiceApiDocs() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Device Service API")
+                        .description("Device service API for Home Energy Tracker Project")
+                        .contact(getContact())
+                        .license(getLicence())
+                        .version("1.0.0"));
+    }
+
+    private static License getLicence() {
+        License license = new License();
+        license.setName("Creative Commons Attribution-NonCommercial 4.0 International License");
+        license.setUrl("https://creativecommons.org/licences/by-nc/4.0/");
+        return license;
+    }
+
+    private static Contact getContact() {
+        Contact contact = new Contact();
+        contact.setUrl("https://kalmejons.com");
+        contact.setEmail("jontyrif@gmail.com");
+        return contact;
+    }
+}
